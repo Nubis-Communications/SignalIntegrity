e51471e99be6025c8b559e44300855e3f591c67f
Pete <pete_pope@hotmail.com>
2015-04-20 20:27:04 -0400

    virtual probe numeric working as intended. not necessarily complete functionality

--- TestSignalIntegrity/TestVirtualProbeNumeric.py
+++ TestSignalIntegrity/TestVirtualProbeNumeric.py
@@ -8,30 +8,37 @@ import copy
 import math
 import os
 
-##import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 class TestVirtualProbeNumeric(unittest.TestCase):
     def testVirtualProbeDC2008(self):
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
         os.chdir('.\\DesignCon2008\\')
-        f=si.sp.EvenlySpacedFrequencyList(10.0,50)
-        #si.sp.ResampledSParameters(si.sp.File('XRAY041.s4p'),si.sp.EvenlySpacedFrequencyList(20.0,400)).WriteToFile('XRAY041.s4p')
-        #return
+        f=si.sp.EvenlySpacedFrequencyList(10.0e9,400)
+##        si.sp.ResampledSParameters(si.sp.File('XRAY041.s4p'),si.sp.EvenlySpacedFrequencyList(20.0e9,400)).WriteToFile('XRAY041.s4p')
+##        return
         vpp=si.p.VirtualProbeNumericParser(f).File('comparison.txt')
         result = vpp.TransferFunctions()
-        ml = vpp.m_ml
-        o1mag = [20.*math.log10(abs(result[n][1][0][0])) for n in range(len(f))]
-        o2mag = [20.*math.log10(abs(result[n][1][1][0])) for n in range(len(f))]
+        ml = vpp.SystemDescription().pMeasurementList
+        ol = vpp.SystemDescription().pOutputList
+        tf=[]
+        tfl=[]
+        for o in range(len(ol)):
+            tfo=[]
+            tfol=[]
+            for m in range(len(ml)):
+                tfo.append([20.*math.log10(abs(result[n][1].tolist()[o][m])) for n in range(len(f))])
+                tfol.append(str(vpp.SystemDescription().pOutputList[o]) + ' due to ' + str(vpp.SystemDescription().pMeasurementList[m]))
+            tf.append(tfo)
+            tfl.append(tfol)
         fp = [ele/1.e9 for ele in f]
-        labels=[]
-        labels.append(str(vpp.SystemDescription().pOutputList[0]) + ' due to ' + str(vpp.SystemDescription().pMeasurementList[0]))
-        labels.append(str(vpp.SystemDescription().pOutputList[1]) + ' due to ' + str(vpp.SystemDescription().pMeasurementList[0]))
-##        plt.xlabel('frequency (GHz)')
-##        plt.ylabel('magnitude (dB)')
-##        plt.plot(fp,o1mag,label=labels[0])
-##        plt.plot(fp,o2mag,label=labels[1])
-##        plt.legend(loc='upper left')
-##        plt.show()
+        plt.xlabel('frequency (GHz)')
+        plt.ylabel('magnitude (dB)')
+        for o in range(len(ol)):
+           for m in range(len(ml)):
+                plt.plot(fp,tf[o][m],label=tfl[o][m])
+        plt.legend(loc='upper right')
+        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
