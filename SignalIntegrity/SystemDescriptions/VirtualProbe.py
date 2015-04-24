b62c36e60725171add5a30755ee99d03dced4420
PeterP <pete_pope@hotmail.com>
2015-04-24 18:12:23 -0400

    squashed virtual probe class to fit better

--- SignalIntegrity/SystemDescriptions/VirtualProbe.py
+++ SignalIntegrity/SystemDescriptions/VirtualProbe.py
@@ -7,57 +7,42 @@ from SignalIntegrity.SystemDescriptions.Device import Device
 class VirtualProbe(SystemSParameters,object):
     def __init__(self,sd):
         SystemSParameters.__init__(self,sd)
-        if hasattr(sd, 'm_ml'):
-            self.m_ml = sd.m_ml
-        else:
-            self.m_ml = None
-        if hasattr(sd, 'm_ol'):
-            self.m_ol = sd.m_ol
-        else:
-            self.m_ol = None
-        if hasattr(sd, 'm_D'):
-            self.m_D = sd.m_D
-        else:
-            self.m_D = None
+        self.m_ml = sd.m_ml if hasattr(sd, 'm_ml') else None
+        self.m_ol = sd.m_ol if hasattr(sd, 'm_ol') else None
+        self.m_D = sd.m_D if hasattr(sd, 'm_D') else None
     @property
     def pMeasurementList(self):
         return self.m_ml
     @pMeasurementList.setter
     def pMeasurementList(self,ml=None):
-        if not ml is None:
-            self.m_ml = ml
+        if not ml is None: self.m_ml = ml
         return self
     @property
     def pOutputList(self):
         return self.m_ol
     @pOutputList.setter
     def pOutputList(self,ol=None):
-        if not ol is None:
-            self.m_ol = ol
+        if not ol is None: self.m_ol = ol
         return self
     @property
     def pStimDef(self):
         return self.m_D
     @pStimDef.setter
     def pStimDef(self,D=None):
-        if not D is None:
-            self.m_D = D
+        if not D is None: self.m_D = D
         return self
     def StimsPrime(self):
         sv=self.StimulusVector()
         sp=[]
         for s in range(len(sv)):
             sn='m'+str(s+1)
-            if sn in sv:
-                sp.append(sn)
-            else:
-                return sp
+            if sn in sv: sp.append(sn)
+            else: return sp
     def SIPrime(self,symbolic=False):
         n=self.NodeVector()
         m=self.StimulusVector()
         mprime=self.StimsPrime()
-        if symbolic:
-            SI=Device.SymbolicMatrix('Si',len(n))
+        if symbolic: SI=Device.SymbolicMatrix('Si',len(n))
         else:
             SI=(matrix(identity(len(n)))-matrix(self.WeightsMatrix())).getI().tolist()
         SiPrime=[[0]*len(mprime) for r in range(len(n))]
