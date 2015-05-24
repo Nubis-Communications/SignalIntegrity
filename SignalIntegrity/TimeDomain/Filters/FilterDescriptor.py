724cd81ded0a1cfaa306d1d4312bcd4be52899eb
PeterP <pete_pope@hotmail.com>
2015-05-24 14:38:14 -0400

    efficient waveform processing
    added original document describing
    added before and after to waveform - untested

--- SignalIntegrity/TimeDomain/Filters/FilterDescriptor.py
+++ SignalIntegrity/TimeDomain/Filters/FilterDescriptor.py
@@ -9,6 +9,16 @@ class FilterDescriptor(object):
                 UpsampleFactor=self.U*other.U,
                 DelaySamples=float(self.U*self.D+other.D)/self.U,
                 StartupSamples=float(self.U*self.S+other.S)/self.U)
+    def Before(self,other):
+        return FilterDescriptor(
+                UpsampleFactor=self.U,
+                DelaySamples=(other.D*other.U+self.D)/other.U-other.D/self.U,
+                StartupSamples=(other.U*other.S+self.S)/other.U-other.S/self.U)
+    def After(self,other):
+        return FilterDescriptor(
+                UpsampleFactor=self.U,
+                DelaySamples=(self.D*self.U+other.D)*other.U/self.U-other.D*other.U,
+                StartupSamples=(self.S*self.U+other.S)*other.U/self.U-other.U*other.S)
     def TrimLeft(self):
         return self.S-self.D
     def TrimRight(self):
