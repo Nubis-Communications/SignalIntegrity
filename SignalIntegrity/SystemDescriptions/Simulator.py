f3295bb5f38647f3bef8f7f7f5fb41ac79651f44
Pete <pete_pope@hotmail.com>
2015-07-13 09:09:42 -0400

    refactored Simulator and VirtualProbe to reverse order of dependency

--- SignalIntegrity/SystemDescriptions/Simulator.py
+++ SignalIntegrity/SystemDescriptions/Simulator.py
@@ -1,30 +1,33 @@
-from SignalIntegrity.SystemDescriptions.VirtualProbe import VirtualProbe
+from numpy import matrix
+from numpy import identity
 
+from SignalIntegrity.SystemDescriptions.SystemSParameters import SystemSParameters
+from SignalIntegrity.SystemDescriptions.Device import Device
 from SignalIntegrity.Devices import Open
 from SignalIntegrity.Devices import Thru
 from SignalIntegrity.Devices import Ground
 
-class Simulator(VirtualProbe,object):
+class Simulator(SystemSParameters,object):
     def __init__(self,sd):
-        VirtualProbe.__init__(self,sd)
-    def _NextStimNumber(self):
-        return len(self.StimsPrime())+1
+        SystemSParameters.__init__(self,sd)
+        self.m_ol = sd.m_ol if hasattr(sd, 'm_ol') else None
+    @property
+    def pOutputList(self):
+        return self.m_ol
+    @pOutputList.setter
+    def pOutputList(self,ol=None):
+        if not ol is None: self.m_ol = ol
+        return self
     def AddVoltageSource(self,Name,Ports):
-        if Ports == 1:
-            self.AddDevice(Name,Ports,Ground(),'voltage source')
-        elif Ports == 2:
-            self.AddDevice(Name,Ports,Thru(),'voltage source')
-        stimNumber=self._NextStimNumber()
-        for p in range(Ports):
-            self.AssignM(Name,p+1,'m'+str(stimNumber+p))
+        if Ports == 1: self.AddDevice(Name,Ports,Ground(),'voltage source')
+        elif Ports == 2: self.AddDevice(Name,Ports,Thru(),'voltage source')
+        stimNumber=len(self.StimsPrime())+1
+        for p in range(Ports): self.AssignM(Name,p+1,'m'+str(stimNumber+p))
     def AddCurrentSource(self,Name,Ports):
-        if Ports == 1:
-            self.AddDevice(Name,Ports,Open(),'current source')
-        elif Ports == 2:
-            self.AddDevice(Name,Ports,[[1.,0.],[0.,1.]],'current source')
-        stimNumber=self._NextStimNumber()
-        for p in range(Ports):
-            self.AssignM(Name,p+1,'m'+str(stimNumber+p))
+        if Ports == 1: self.AddDevice(Name,Ports,Open(),'current source')
+        elif Ports == 2: self.AddDevice(Name,Ports,[[1.,0.],[0.,1.]],'current source')
+        stimNumber=len(self.StimsPrime())+1
+        for p in range(Ports): self.AssignM(Name,p+1,'m'+str(stimNumber+p))
     def SourceVector(self):
         sv=[]
         for d in self:
@@ -51,3 +54,30 @@ class Simulator(VirtualProbe,object):
                     sm[sp.index(d[0].pM)][sv.index(s)] = -1./2.
                     sm[sp.index(d[1].pM)][sv.index(s)] = 1./2.
         return sm
+    def StimsPrime(self):
+        sv=self.StimulusVector()
+        sp=[]
+        for s in range(len(sv)):
+            sn='m'+str(s+1)
+            if sn in sv: sp.append(sn)
+            else: return sp
+    def SIPrime(self,symbolic=False):
+        n=self.NodeVector()
+        m=self.StimulusVector()
+        mprime=self.StimsPrime()
+        if symbolic: SI=Device.SymbolicMatrix('Si',len(n))
+        else:
+            SI=(matrix(identity(len(n)))-matrix(self.WeightsMatrix())).getI().tolist()
+        SiPrime=[[0]*len(mprime) for r in range(len(n))]
+        for c in range(len(mprime)):
+            for r in range(len(n)):
+                SiPrime[r][c]=SI[r][m.index('m'+str(c+1))]
+        return SiPrime
+    def VoltageExtractionMatrix(self,nl):
+        n=self.NodeVector()
+        result=[[0]*len(n) for r in range(len(nl))]
+        for r in range(len(nl)):
+            dp=self[self.DeviceNames().index(nl[r][0])][nl[r][1]-1]
+            result[r][n.index(dp.pA)]=1
+            result[r][n.index(dp.pB)]=1
+        return result
--- SignalIntegrity/SystemDescriptions/VirtualProbe.py
+++ SignalIntegrity/SystemDescriptions/VirtualProbe.py
@@ -1,14 +1,9 @@
-from numpy import matrix
-from numpy import identity
+from SignalIntegrity.SystemDescriptions.Simulator import Simulator
 
-from SignalIntegrity.SystemDescriptions.SystemSParameters import SystemSParameters
-from SignalIntegrity.SystemDescriptions.Device import Device
-
-class VirtualProbe(SystemSParameters,object):
+class VirtualProbe(Simulator,object):
     def __init__(self,sd):
-        SystemSParameters.__init__(self,sd)
+        Simulator.__init__(self,sd)
         self.m_ml = sd.m_ml if hasattr(sd, 'm_ml') else None
-        self.m_ol = sd.m_ol if hasattr(sd, 'm_ol') else None
         self.m_D = sd.m_D if hasattr(sd, 'm_D') else None
     @property
     def pMeasurementList(self):
@@ -18,43 +13,9 @@ class VirtualProbe(SystemSParameters,object):
         if not ml is None: self.m_ml = ml
         return self
     @property
-    def pOutputList(self):
-        return self.m_ol
-    @pOutputList.setter
-    def pOutputList(self,ol=None):
-        if not ol is None: self.m_ol = ol
-        return self
-    @property
     def pStimDef(self):
         return self.m_D
     @pStimDef.setter
     def pStimDef(self,D=None):
         if not D is None: self.m_D = D
         return self
-    def StimsPrime(self):
-        sv=self.StimulusVector()
-        sp=[]
-        for s in range(len(sv)):
-            sn='m'+str(s+1)
-            if sn in sv: sp.append(sn)
-            else: return sp
-    def SIPrime(self,symbolic=False):
-        n=self.NodeVector()
-        m=self.StimulusVector()
-        mprime=self.StimsPrime()
-        if symbolic: SI=Device.SymbolicMatrix('Si',len(n))
-        else:
-            SI=(matrix(identity(len(n)))-matrix(self.WeightsMatrix())).getI().tolist()
-        SiPrime=[[0]*len(mprime) for r in range(len(n))]
-        for c in range(len(mprime)):
-            for r in range(len(n)):
-                SiPrime[r][c]=SI[r][m.index('m'+str(c+1))]
-        return SiPrime
-    def VoltageExtractionMatrix(self,nl):
-        n=self.NodeVector()
-        result=[[0]*len(n) for r in range(len(nl))]
-        for r in range(len(nl)):
-            dp=self[self.DeviceNames().index(nl[r][0])][nl[r][1]-1]
-            result[r][n.index(dp.pA)]=1
-            result[r][n.index(dp.pB)]=1
-        return result
