756fff2223fc1650903e6a255f16f3c178d1ee2c
Pete <pete_pope@hotmail.com>
2015-07-12 20:22:19 -0400

    More Simulation Chapter

---
+++ TestSignalIntegrity/SymbolicSimulatorExample1aCode.py
@@ -0,0 +1,17 @@
+import SignalIntegrity as si
+
+sd=si.sd.SystemDescription()
+sd.AddDevice('S',2)
+sd.AddDevice('\\Gamma_l',1)
+sd.ConnectDevicePort('S',2,'\\Gamma_l',1)
+s=si.sd.Simulator(sd)
+s.AddVoltageSource('V',1)
+s.AddDevice('Zs',2)
+s.ConnectDevicePort('V',1,'Zs',1)
+s.ConnectDevicePort('Zs',2,'S',1)
+s.pOutputList = [('S',1),('S',2)]
+ss=si.sd.SimulatorSymbolic(s)
+ss.LaTeXEquations().Emit()
+ss.AssignSParameters('Zs',si.sy.SeriesZ('Zs'))
+ss.AssignSParameters('\\Gamma_l',si.sy.ShuntZ(1,'Zl'))
+ss.Clear().LaTeXEquations().Emit()
--- TestSignalIntegrity/TestSimulator.py
+++ TestSignalIntegrity/TestSimulator.py
@@ -33,15 +33,38 @@ class Test(unittest.TestCase,RoutineWriterTesterHelper,ResponseTesterHelper,Sour
         ssp=si.sd.SystemSParametersSymbolic(sd)
         ssp.LaTeXSystemEquation().Emit()
         # exclude
-        self.CheckSymbolicResult(self.id()+'_1',ssp,'Symbolic Simulator 1')
+        self.CheckSymbolicResult(self.id()+'_1',ssp,self.id())
         # include
         ssp.AssignSParameters('\\Gamma_s',si.sy.ShuntZ(1,'Zs'))
         ssp.AssignSParameters('\\Gamma_l',si.sy.ShuntZ(1,'Zl'))
         ssp.Clear().LaTeXSystemEquation().Emit()
         # exclude
-        self.CheckSymbolicResult(self.id()+'_2',ssp,'Symbolic Simulator 1')
+        self.CheckSymbolicResult(self.id()+'_2',ssp,self.id())
     def testSymbolicSimulatorExample1Code(self):
         self.WriteCode('TestSimulator.py','testSymbolicSimulatorExample1(self)',self.standardHeader)
+    def testSymbolicSimulatorExample1a(self):
+        sd=si.sd.SystemDescription()
+        sd.AddDevice('S',2)
+        sd.AddDevice('\\Gamma_l',1)
+        sd.ConnectDevicePort('S',2,'\\Gamma_l',1)
+        s=si.sd.Simulator(sd)
+        s.AddVoltageSource('V',1)
+        s.AddDevice('Zs',2)
+        s.ConnectDevicePort('V',1,'Zs',1)
+        s.ConnectDevicePort('Zs',2,'S',1)
+        s.pOutputList = [('S',1),('S',2)]
+        ss=si.sd.SimulatorSymbolic(s)
+        ss.LaTeXEquations().Emit()
+        # exclude
+        self.CheckSymbolicResult(self.id()+'_1',ss,self.id())
+        # include
+        ss.AssignSParameters('Zs',si.sy.SeriesZ('Zs'))
+        ss.AssignSParameters('\\Gamma_l',si.sy.ShuntZ(1,'Zl'))
+        ss.Clear().LaTeXEquations().Emit()
+        # exclude
+        self.CheckSymbolicResult(self.id()+'_2',ss,self.id())
+    def testSymbolicSimulatorExample1aCode(self):
+        self.WriteCode('TestSimulator.py','testSymbolicSimulatorExample1a(self)',self.standardHeader)
     def testSymbolicSimulatorExample3(self):
         sd=si.sd.SystemDescription()
         sd.AddDevice('C',4)
