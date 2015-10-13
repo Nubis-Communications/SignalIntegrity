f833758483c4eba030a6c94b1dcd457bd4eeaca3
Pete <pete_pope@hotmail.com>
2015-10-13 19:55:40 -0400

    more parts picking plus hires scope testing

--- PySIApp/PySIApp.py
+++ PySIApp/PySIApp.py
@@ -2,15 +2,56 @@ from Tkinter import *
 import ttk
 from tkFileDialog import askopenfilename
 from tkFileDialog import asksaveasfilename
-from libxml2mod import properties
 import keyword
+import copy
+
+class PartGeometry(object):
+    def __init__(self,portsCoordinateList):
+        self.x=0
+        self.y=0
+        self.portsCoordinateList=portsCoordinateList
+    def SetOrigin(self,x,y,grid):
+        xord = int(round(x/grid))
+        yord = int(round(y/grid))
+        self.x=xord
+        self.y=yord
+    def DrawDevice(self,canvas,grid):
+        print 'drawing device at: ',self.x,self.y
+        minx=self.portsCoordinateList[0][0]
+        maxx=self.portsCoordinateList[0][0]
+        miny=self.portsCoordinateList[0][1]
+        maxy=self.portsCoordinateList[0][1]
+        for coord in self.portsCoordinateList:
+            if coord[0]<minx: minx=coord[0]
+            if coord[0]>maxx: maxx=coord[0]
+            if coord[1]<miny: miny=coord[1]
+            if coord[1]>maxy: maxy=coord[1]
+        boundingBoxInitial=[minx,miny]
+        boundingBoxFinal=[maxx,maxy]
+        innerBoxInitial=[minx+1,miny+1]
+        innerBoxFinal=[maxx-1,maxy-1]
+        canvas.create_rectangle(innerBoxInitial[0]*grid+self.x*grid,innerBoxInitial[1]*grid+self.y*grid,innerBoxFinal[0]*grid+self.x*grid,innerBoxFinal[1]*grid+self.y*grid)
+        for pin in self.portsCoordinateList:
+            if pin[0]==boundingBoxInitial[0]: # pin on left side
+                canvas.create_line(pin[0]*grid+self.x*grid,pin[1]*grid+self.y*grid,(pin[0]+1)*grid+self.x*grid,pin[1]*grid+self.y*grid)
+            elif pin[0]==boundingBoxFinal[0]: # pin on right side
+                canvas.create_line(pin[0]*grid+self.x*grid,pin[1]*grid+self.y*grid,(pin[0]-1)*grid+self.x*grid,pin[1]*grid+self.y*grid)
+
+
+class PartGeometryTwoPort(PartGeometry):
+    def __init__(self):
+        PartGeometry.__init__(self,[[0,1],[4,1]])
+
+class PartGeometryNone(PartGeometry):
+    def __init__(self):
+        PartGeometry.__init__(self,None)
 
 class Property(object):
     def __init__(self,keyword=None,description=None,value=None):
         self.keyword=keyword
         self.description=description
         self.value=value
-        
+
 class Device(object):
     def __init__(self,name='',description='',ports=None,category=None,propertiesList=None):
         self.name=name
@@ -18,6 +59,12 @@ class Device(object):
         self.ports=ports
         self.category=category
         self.propertiesList=propertiesList
+        if self.ports == 2:
+            self.partGeometry = PartGeometryTwoPort()
+        else:
+            self.partGeometry = PartGeometryNone()
+    def DrawDevice(self,canvas,grid):
+        self.partGeometry.DrawDevice(canvas,grid)
 
 DeviceList = [
               Device(name='File',description='One\ Port\ File',ports=1,category='Files',propertiesList=[Property(keyword='',description='file name',value='')]),
@@ -31,9 +78,9 @@ DeviceList = [
               Device(name='Inductor',description='One\ Port\ Inductor\ to\ Ground',ports=1,category='Inductors',propertiesList=[Property(keyword='l',description='inductance (H)',value=1e-15)]),
               Device(name='Inductor',description='Two\ Port\ Inductor',ports=2,category='Inductors',propertiesList=[Property(keyword='l',description='inductance (H)',value=1e-15)]),
               Device(name='Mutual',description='Four\ Port\ Mutual\ Inductance',ports=4,category='Inductors',propertiesList=[Property(keyword='l',description='inductance (H)',value=1e-15)])
-              ]            
-# 
-#         
+              ]
+#
+#
 # DeviceList = [
 #               ['File','One\ Port\ File',1,'Files',[['','file name','']]],
 #               ['File','Two\ Port\ File',2,'Files',[['','file name','']]],
@@ -46,7 +93,7 @@ DeviceList = [
 #               ['Inductor','One\ Port\ Inductor\ to\ Ground',1,'Inductors',[['c','inductance (H)',1e-15]]],
 #               ['Inductor','Two\ Port\ Inductor',2,'Inductors',[['c','inductance (H)',1e-15]]],
 #               ['Mutual','Four\ Port\ Mutual\ Inductance',4,'Inductors',[['m','mutual inductance (H)',1e-15]]]
-#               ]            
+#               ]
 
 class DeviceProperties(Frame):
     def __init__(self,parent,device):
@@ -54,7 +101,7 @@ class DeviceProperties(Frame):
         self.title = 'Add '+device.name
         self.properties=[Property(keyword='',description='ports',value=device.ports)]+device.propertiesList
         self.propertyStrings=[StringVar(value=str(prop.value)) for prop in self.properties]
-        
+
         for p in range(len(self.properties)):
             prop=self.properties[p]
             propertyFrame = Frame(self)
@@ -69,6 +116,8 @@ class DevicePropertiesDialog(Toplevel):
     def __init__(self,parent,device):
         Toplevel.__init__(self, parent)
         self.transient(parent)
+
+        self.device = device
 
         self.title('Add '+device.description)
 
@@ -150,9 +199,8 @@ class DevicePropertiesDialog(Toplevel):
         return 1 # override
 
     def apply(self):
-        if (self.DevicePicker.selected != None):
-            if self.DevicePicker.selected != 'category':
-                self.result = int(self.DevicePicker.selected)
+        self.result=copy.deepcopy(self.device)
+        self.result.propertiesList=[Property(prop.keyword,prop.description,prop.value) for prop in self.device.propertiesList]
 
 class DevicePicker(Frame):
     def __init__(self,parent):
@@ -278,6 +326,10 @@ class DeviceFrame(Frame):
         self.Picker = DevicePicker(self)
         self.Picker.pack()
 
+class Schematic(object):
+    def __init__(self):
+        self.deviceList = []
+
 class SchematicFrame(Frame):
     def __init__(self,parent):
         Frame.__init__(self,parent)
@@ -285,12 +337,32 @@ class SchematicFrame(Frame):
         self.canvas.pack(side=TOP, fill=BOTH, expand=YES)
         self.canvas.create_rectangle(1,1,self.canvas.winfo_reqheight(),self.canvas.winfo_reqwidth())
         self.canvas.bind("<Configure>", self.on_resize)
+        self.canvas.bind('<Button-1>',self.onMouseButton1)
+        self.grid=10
+        self.partLoaded = None
+        self.schematic = Schematic()
     def on_resize(self,event):
         self.canvas.delete(ALL)
         self.canvas.create_rectangle(1,1,event.width-5,event.height-5)
-        
+        self.DrawSchematic()
+    def onMouseButton1(self,event):
+        print 'clicked at: ',event.x,event.y
+        if not self.partLoaded == None:
+            partToLoad=self.partLoaded
+            partToLoad.partGeometry.SetOrigin(event.x,event.y,self.grid)
+            self.schematic.deviceList.append(partToLoad)
+            self.partLoaded=None
+            for device in self.schematic.deviceList:
+                print device.partGeometry.x,device.partGeometry.y
+        self.DrawSchematic()
+    def DrawSchematic(self):
+        self.canvas.delete(ALL)
+        for device in self.schematic.deviceList:
+            device.DrawDevice(self.canvas,self.grid)
+
+
 class TheApp(Frame):
-    def __init__(self): 
+    def __init__(self):
         root = Tk()
         Frame.__init__(self, root)
         self.pack(fill=BOTH, expand=YES)
@@ -305,10 +377,15 @@ class TheApp(Frame):
         FileMenu.add_command(label="Write Schematic",command=self.onWriteSchematic)
         FileMenu.add_separator()
         FileMenu.add_command(label="Export NetList",command=self.onExportNetlist)
-        
+
         PartsMenu=Menu(menu)
         menu.add_cascade(label='Parts',menu=PartsMenu)
         PartsMenu.add_command(label='Add Part',command=self.onAddPart)
+
+        ZoomMenu=Menu(menu)
+        menu.add_cascade(label='Zoom',menu=ZoomMenu)
+        ZoomMenu.add_command(label='Zoom In',command=self.onZoomIn)
+        ZoomMenu.add_command(label='Zoom Out',command=self.onZoomOut)
 
 #         self.DeviceFrame = DeviceFrame(self)
 #         self.DeviceFrame.pack(side=LEFT, fill=Y, expand=NO)
@@ -318,8 +395,6 @@ class TheApp(Frame):
 #         self.DeviceFrame.Picker.tree.bind('<<TreeviewSelect>>',self.onPartSelection)
         root.mainloop()
 
-        
-        
     def onReadSchematic(self):
         pass
 
@@ -328,13 +403,23 @@ class TheApp(Frame):
 
     def onExportNetlist(self):
         pass
-    
+
     def onAddPart(self):
         dpd=DevicePickerDialog(self)
         if dpd.result != None:
             dpe=DevicePropertiesDialog(self,DeviceList[dpd.result])
-        pass
-        
+            self.SchematicFrame.partLoaded = dpe.result
+
+    def onZoomIn(self):
+        self.SchematicFrame.grid = self.SchematicFrame.grid*2
+        self.SchematicFrame.DrawSchematic()
+
+    def onZoomOut(self):
+        self.SchematicFrame.grid = max(1,self.SchematicFrame.grid/2)
+        self.SchematicFrame.DrawSchematic()
+
+
+
 def main():
     app=TheApp()
 
---
+++ TestSignalIntegrity/TestHiRes.py
@@ -0,0 +1,201 @@
+import unittest
+import SignalIntegrity as si
+from TestHelpers import *
+import numpy as np
+import math
+
+class TestSimulator(unittest.TestCase,RoutineWriterTesterHelper,ResponseTesterHelper,SourcesTesterHelper):
+    def __init__(self, methodName='runTest'):
+        RoutineWriterTesterHelper.__init__(self)
+        unittest.TestCase.__init__(self,methodName)
+
+
+    def testHiRes2(self):
+        # exclude
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        # include
+        Fsa=40e9    # assumed analog sample rate
+        NoiseBandwidth=10e9
+        snrdB = 42.5
+        #snrdB = 100
+
+        # Make a filter with bandwidth equal to the noise bandwidth from 6 GHz filter s-parameters
+        OriginalFilterBandwidth = 6e9
+        sp = si.sp.File('filter.s2p')
+        sp.m_f=si.fd.EvenlySpacedFrequencyList(NoiseBandwidth/OriginalFilterBandwidth*sp.f()[-1],len(sp.f())-1)
+        sp.WriteToFile('filterNBW.s2p')
+        del OriginalFilterBandwidth
+        del sp
+        del NoiseBandwidth
+
+        # make a simulator system to filter the noise according to the noise bandwidth
+        snp=si.p.SimulatorNumericParser(si.fd.EvenlySpacedFrequencyList(Fsa/2,2000))
+        snp.AddLine('device F 2 file filterNBW.s2p')
+        snp.AddLine('device S 2 R 50.')
+        snp.AddLine('device R 1 R 50.')
+        snp.AddLine('voltagesource V 1')
+        snp.AddLine('connect V 1 S 1')
+        snp.AddLine('connect S 2 F 1')
+        snp.AddLine('connect F 2 R 1')
+        snp.AddLine('output R 1')
+        tmp=si.td.f.TransferMatricesProcessor(snp.TransferMatrices())
+        del snp
+
+        # make the analog waveforms
+        K=2000
+        KaddBothSides=8000
+        fin=100e6
+        vdiv=50e-3
+        Amplitude=4*vdiv
+        sigmafe=4.*Amplitude/math.sqrt(2.)*math.pow(10.,-snrdB/20.)
+        td=si.td.wf.TimeDescriptor(-KaddBothSides/Fsa,K+KaddBothSides*2,Fsa)
+        wfa=si.td.wf.Waveform(td,[Amplitude*math.sin(2.*math.pi*fin*t) for t in td.Times()])
+        wfna=si.td.wf.Waveform(td,np.random.normal(0,sigmafe,td.N).tolist())
+        del KaddBothSides
+        del fin
+        del vdiv
+        del td
+        del t
+
+        # wfa and wfna are analog and noise waveforms
+        # process these waveforms through the noise filter
+        wfa=tmp.ProcessWaveforms([wfa*2.])[0]
+        wfna=tmp.ProcessWaveforms([wfna*2.])[0]
+        del tmp
+
+        # now we need to measure the noise waveform and adjust the noise to match
+        # what is stated
+        sigmafeActual =np.std(wfna.Values())
+        wfna=si.td.wf.Waveform(wfna.TimeDescriptor(),[wfna.Values()[k]*sigmafe/sigmafeActual for k in range(len(wfna.Values()))])
+        sigmafeActual=np.std(wfna.Values())
+        del sigmafeActual
+        del k
+
+        # wfa and wfna are analog waveforms
+        # wfa is the analog input waveform
+        # wfna is an analog noise waveform that meets the noise characteristics
+        # measured without a hardware filter
+
+        # adapt both waveforms to a desired scale
+        #td=si.td.wf.TimeDescriptor(0,K,Fsa)
+        #wfna=wfna.Adapt(td)
+        #wfa=wfa.Adapt(td)
+
+        HWFilterBandwidth = 4e9 # what we want the bandwidth to be
+
+        # 6 GHz filter s-parameters
+        OriginalFilterBandwidth = 6e9
+        sp = si.sp.File('filter.s2p')
+        sp.m_f=si.fd.EvenlySpacedFrequencyList(HWFilterBandwidth/OriginalFilterBandwidth*sp.f()[-1],len(sp.f())-1)
+        sp.WriteToFile('filterBWL.s2p')
+        del OriginalFilterBandwidth
+        del sp
+        del HWFilterBandwidth
+
+        # make a simulator system to filter the noise according to the noise bandwidth
+        snp=si.p.SimulatorNumericParser(si.fd.EvenlySpacedFrequencyList(Fsa/2,2000))
+        snp.AddLine('device F 2 file filterBWL.s2p')
+        snp.AddLine('device S 2 R 50.')
+        snp.AddLine('device R 1 R 50.')
+        snp.AddLine('voltagesource V 1')
+        snp.AddLine('connect V 1 S 1')
+        snp.AddLine('connect S 2 F 1')
+        snp.AddLine('connect F 2 R 1')
+        snp.AddLine('output R 1')
+        tmp=si.td.f.TransferMatricesProcessor(snp.TransferMatrices())
+        del snp
+
+        wfnafe=tmp.ProcessWaveforms([wfna*2.])[0]
+        wf=tmp.ProcessWaveforms([wfa*2.])[0]
+        del tmp
+
+        # wfnafe and wf are analog waveforms
+        # wfnafe is now the noise waveform that has been filtered by a hardware
+        # filter
+        # wf is the analog input waveform filtered by the hardware filter
+
+        Fs=40e9 # system sample rate
+
+        snrdBadc=46
+        #snrdBadc=90
+
+        sigmaadc=4.*Amplitude/math.sqrt(2.)*math.pow(10.,-snrdBadc/20.)
+        wfnadc=si.td.wf.Waveform(wfnafe.TimeDescriptor(),np.random.normal(0,sigmaadc,wfnafe.TimeDescriptor().N).tolist())
+        sigmaadcActual = np.std(wfnadc.Values())
+        wfnadc=si.td.wf.Waveform(wfnafe.TimeDescriptor(),[wfnadc.Values()[k]*sigmaadc/sigmaadcActual for k in range(len(wfnadc.Values()))])
+        sigmaadcActual = np.std(wfnadc.Values())
+        del sigmaadc
+        del sigmaadcActual
+        del k
+
+        # now we have wf, wfnafe, and wfnadc
+        # these are the analog waveform, the analog front-end noise and
+        # the adc noise (assumed white)
+
+        wfn=wfnafe+wfnadc
+        wfs=wf+wfn
+
+        # form the decimated waveforms
+        VILV=4
+
+        wfd = [si.td.wf.Waveform(si.td.wf.TimeDescriptor(wf.Times()[d],len(wf)/VILV,wf.TimeDescriptor().Fs/VILV),[wf.Values()[kd*VILV+d] for kd in range(len(wf)/VILV)]) for d in range(VILV)]
+        wfnd = [si.td.wf.Waveform(si.td.wf.TimeDescriptor(wfn.Times()[d],len(wfn)/VILV,wfn.TimeDescriptor().Fs/VILV),[wfn.Values()[kd*VILV+d] for kd in range(len(wfn)/VILV)]) for d in range(VILV)]
+        wfsd = [si.td.wf.Waveform(si.td.wf.TimeDescriptor(wfs.Times()[d],len(wfs)/VILV,wfs.TimeDescriptor().Fs/VILV),[wfs.Values()[kd*VILV+d] for kd in range(len(wfs)/VILV)]) for d in range(VILV)]
+
+        wfavg=wfd[0]*(1./VILV)
+        wfnavg=wfnd[0]*(1./VILV)
+        wfsavg=wfsd[0]*(1./VILV)
+        for d in range(1,VILV):
+            wfavg=wfavg+wfd[d]*(1./VILV)
+            wfnavg=wfnavg+wfnd[d]*(1./VILV)
+            wfsavg=wfsavg+wfsd[d]*(1./VILV)
+
+        us=si.td.f.InterpolatorSinX(VILV)
+
+        wfavgus = us.FilterWaveform(wfavg)
+        wfnavgus = us.FilterWaveform(wfnavg)
+        wfsavgus = us.FilterWaveform(wfsavg)
+
+        #adapt all of the waveform results
+        wf=wf.Adapt(si.td.wf.TimeDescriptor(0.,K/Fs*wf.TimeDescriptor().Fs,wf.TimeDescriptor().Fs))
+        wfs=wfs.Adapt(si.td.wf.TimeDescriptor(0.,K/Fs*wfs.TimeDescriptor().Fs,wfs.TimeDescriptor().Fs))
+        wfsavgus=wfsavgus.Adapt(si.td.wf.TimeDescriptor(0.,K/Fs*wfsavgus.TimeDescriptor().Fs,wfsavgus.TimeDescriptor().Fs))
+
+        wfnavgus=wfnavgus.Adapt(si.td.wf.TimeDescriptor(0.,K/Fs*wfnavgus.TimeDescriptor().Fs,wfnavgus.TimeDescriptor().Fs))
+        wfn=wfn.Adapt(si.td.wf.TimeDescriptor(0.,K/Fs*wfn.TimeDescriptor().Fs,wfn.TimeDescriptor().Fs))
+
+        if True:
+            import matplotlib.pyplot as plt
+            plt.clf()
+            plt.xlabel('time (ns)')
+            plt.ylabel('amplitude')
+            plt.plot(wf.Times('ns'),wf.Values(),label='input waveform')
+            plt.plot(wfs.Times('ns'),wfs.Values(),label='acquired at 40 GS/s input')
+            #for d in range(VILV):
+            #    plt.plot(wfad[d].Times('ns'),wfad[d].Values(),label='10 GS/s stream '+str(d))
+            #plt.plot(wfavg.Times('ns'),wfavg.Values(),label='averaged 10 GS/s streams')
+            plt.plot(wfsavgus.Times('ns'),wfsavgus.Values(),label='upsampled averaged streams')
+            plt.legend(loc='upper right')
+            plt.show()
+
+        if True:
+            import matplotlib.pyplot as plt
+            plt.clf()
+            plt.xlabel('time (ns)')
+            plt.ylabel('amplitude')
+            plt.plot(wfn.Times('ns'),wfn.Values(),label='noise acquired at 40 GS/s input')
+            plt.plot(wfnavgus.Times('ns'),wfnavgus.Values(),label='noise after averaging and upsampling')
+            plt.legend(loc='upper right')
+            plt.show()
+
+        sigmai=np.std(wfn.Values())
+        snri=20*math.log10(4.*Amplitude/math.sqrt(2.)/sigmai)
+        enobi=(snri-1.76)/6.02
+        print snri,enobi
+        sigmaf=np.std(wfnavgus.Values())
+        snrf=20*math.log10(4.*Amplitude/math.sqrt(2.)/sigmaf)
+        enobf=(snrf-1.76)/6.02
+        print snrf,enobf
+
+if __name__ == "__main__":
+    unittest.main()
