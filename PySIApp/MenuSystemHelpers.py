4ea376416ed76a224629cbd99579543a477672c4
Pete <pete_pope@hotmail.com>
2015-12-02 16:57:47 -0500

    Consolidated things - moved menu and toolbar back into main app Put Doer related stuff in a helper file so I don't have to look at it all the time.

---
+++ PySIApp/MenuSystemHelpers.py
@@ -0,0 +1,112 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      peter.pupalaikis
+#
+# Created:     02/12/2015
+# Copyright:   (c) peter.pupalaikis 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from Tkinter import *
+
+class MenuElement(object):
+    def __init__(self,menu,**kw):
+        menu.add_command(kw)
+        self.label=kw['label']
+        self.menu=menu
+        self.active=None
+        if 'state' in kw:
+            active=kw['state'] == 'normal'
+        else:
+            active=True
+        self.Activate(active)
+    def Activate(self,active):
+        if active == self.active:
+            return
+        self.active=active
+        self.menu.entryconfigure(self.label,state='normal' if self.active else 'disabled')
+
+class ToolBarElement(object):
+    def __init__(self,frame,**kw):
+        if 'iconfile' in kw:
+            self.icon = PhotoImage(file=kw['iconfile'])
+            del kw['iconfile']
+            kw['image']=self.icon
+        self.button=Button(frame,kw)
+        self.active=None
+        if 'state' in kw:
+            active=kw['state'] == 'normal'
+        else:
+            active=True
+        self.Activate(active)
+    def Activate(self,active):
+        if active == self.active:
+            return
+        self.active=active
+        self.button.config(state='normal' if self.active else 'disabled')
+    def Pack(self,**kw):
+        self.button.pack(kw)
+        return self
+
+class KeyBindElement(object):
+    def __init__(self,bindTo,key,func):
+        self.bindTo=bindTo
+        self.key=key
+        self.func=func
+        self.active=False
+    def Activate(self,active):
+        if active == self.active:
+            return
+        self.active=active
+        if self.active:
+            self.bindTo.bind(self.key,self.func)
+        else:
+            self.bindTo.unbind(self.key)
+
+class Doer(object):
+    def __init__(self,command,active=True):
+        self.active=active
+        self.command=command
+        self.menuElement=None
+        self.toolBarElement=None
+        self.keyBindElement=None
+    def AddMenuElement(self,menu,**kw):
+        kw['command']=self.Execute
+        self.menuElement=MenuElement(menu,**kw)
+        self.menuElement.Activate(self.active)
+        return self.menuElement
+    def AddToolBarElement(self,frame,**kw):
+        kw['command']=self.Execute
+        self.toolBarElement=ToolBarElement(frame,**kw)
+        self.toolBarElement.Activate(self.active)
+        return self.toolBarElement
+    def AddKeyBindElement(self,bindTo,key):
+        self.keyBindElement = KeyBindElement(bindTo,key,self.Execute)
+        self.keyBindElement.Activate(self.active)
+        return self
+    def Execute(self,*args):
+        if self.active:
+            return self.command()
+    def Activate(self,active):
+        if self.active==active:
+            return
+        self.active=active
+        if self.menuElement is not None:
+            self.menuElement.Activate(active)
+        if self.toolBarElement is not None:
+            self.toolBarElement.Activate(active)
+        if self.keyBindElement is not None:
+            self.keyBindElement.Activate(active)
+
+class StatusBar(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.label = Label(self, bd=1, relief=SUNKEN, anchor=W)
+        self.label.pack(fill=X)
+    def set(self, format, *args):
+        self.label.config(text=format % args)
+        self.label.update_idletasks()
+    def clear(self):
+        self.label.config(text="")
+        self.label.update_idletasks()
--- PySIApp/PySIApp.py
+++ PySIApp/PySIApp.py
@@ -7,8 +7,6 @@ import tkMessageBox
 import copy
 import os
 
-#from idlelib.ToolTip import *
-
 from PartPin import *
 from PartPicture import *
 from PartProperty import *
@@ -23,193 +21,7 @@ from NetList import *
 from SParameterViewerWindow import *
 from Files import *
 from History import *
-
-class MenuElement(object):
-    def __init__(self,menu,**kw):
-        menu.add_command(kw)
-        self.label=kw['label']
-        self.menu=menu
-        self.active=None
-        if 'state' in kw:
-            active=kw['state'] == 'normal'
-        else:
-            active=True
-        self.Activate(active)
-    def Activate(self,active):
-        if active == self.active:
-            return
-        self.active=active
-        self.menu.entryconfigure(self.label,state='normal' if self.active else 'disabled')
-
-class ToolBarElement(object):
-    def __init__(self,frame,**kw):
-        if 'iconfile' in kw:
-            self.icon = PhotoImage(file=kw['iconfile'])
-            del kw['iconfile']
-            kw['image']=self.icon
-        self.button=Button(frame,kw)
-        self.active=None
-        if 'state' in kw:
-            active=kw['state'] == 'normal'
-        else:
-            active=True
-        self.Activate(active)
-    def Activate(self,active):
-        if active == self.active:
-            return
-        self.active=active
-        self.button.config(state='normal' if self.active else 'disabled')
-    def Pack(self,**kw):
-        self.button.pack(kw)
-        return self
-
-class KeyBindElement(object):
-    def __init__(self,bindTo,key,func):
-        self.bindTo=bindTo
-        self.key=key
-        self.func=func
-        self.active=False
-    def Activate(self,active):
-        if active == self.active:
-            return
-        self.active=active
-        if self.active:
-            self.bindTo.bind(self.key,self.func)
-        else:
-            self.bindTo.unbind(self.key)
-
-class Doer(object):
-    def __init__(self,command,active=True):
-        self.active=active
-        self.command=command
-        self.menuElement=None
-        self.toolBarElement=None
-        self.keyBindElement=None
-    def AddMenuElement(self,menu,**kw):
-        kw['command']=self.Execute
-        self.menuElement=MenuElement(menu,**kw)
-        self.menuElement.Activate(self.active)
-        return self.menuElement
-    def AddToolBarElement(self,frame,**kw):
-        kw['command']=self.Execute
-        self.toolBarElement=ToolBarElement(frame,**kw)
-        self.toolBarElement.Activate(self.active)
-        return self.toolBarElement
-    def AddKeyBindElement(self,bindTo,key):
-        self.keyBindElement = KeyBindElement(bindTo,key,self.Execute)
-        self.keyBindElement.Activate(self.active)
-        return self
-    def Execute(self,*args):
-        if self.active:
-            return self.command()
-    def Activate(self,active):
-        if self.active==active:
-            return
-        self.active=active
-        if self.menuElement is not None:
-            self.menuElement.Activate(active)
-        if self.toolBarElement is not None:
-            self.toolBarElement.Activate(active)
-        if self.keyBindElement is not None:
-            self.keyBindElement.Activate(active)
-
-class TheMenu(Menu):
-    def __init__(self,parent):
-        self.parent=parent
-        Menu.__init__(self,self.parent.root)
-        self.parent.root.config(menu=self)
-        FileMenu=Menu(self)
-        self.add_cascade(label='File',menu=FileMenu,underline=0)
-        self.parent.OpenProjectDoer.AddMenuElement(FileMenu,label="Open Project",accelerator='Ctrl+O',underline=0)
-        self.parent.SaveProjectDoer.AddMenuElement(FileMenu,label="Save Project",accelerator='Ctrl+S',underline=0)
-        FileMenu.add_separator()
-        self.parent.ClearProjectDoer.AddMenuElement(FileMenu,label="Clear Schematic",underline=0)
-        FileMenu.add_separator()
-        self.parent.ExportNetListDoer.AddMenuElement(FileMenu,label="Export NetList",underline=7)
-        self.parent.ExportTpXDoer.AddMenuElement(FileMenu,label="Export LaTeX (TikZ)",underline=7)
-
-        EditMenu=Menu(self)
-        self.add_cascade(label='Edit',menu=EditMenu,underline=0)
-        self.parent.UndoDoer.AddMenuElement(EditMenu,label="Undo",accelerator='Ctrl+Z', underline=0)
-        self.parent.RedoDoer.AddMenuElement(EditMenu,label="Redo",accelerator='Ctrl+Shift+Z',underline=0)
-        EditMenu.add_separator()
-
-        PartsMenu=Menu(self)
-        self.add_cascade(label='Parts',menu=PartsMenu,underline=0)
-        self.parent.AddPartDoer.AddMenuElement(PartsMenu,label='Add Part',underline=0)
-        self.parent.AddPortDoer.AddMenuElement(PartsMenu,label='Add Port',underline=5)
-        PartsMenu.add_separator()
-        self.parent.DeletePartDoer.AddMenuElement(PartsMenu,label='Delete Part',underline=0)
-        PartsMenu.add_separator()
-        self.parent.EditPropertiesDoer.AddMenuElement(PartsMenu,label='Edit Properties',underline=0)
-        self.parent.DuplicatePartDoer.AddMenuElement(PartsMenu,label='Duplicate Part',accelerator='Ctrl+C',underline=0)
-        self.parent.RotatePartDoer.AddMenuElement(PartsMenu,label='Rotate Part',underline=0)
-        self.parent.FlipPartHorizontallyDoer.AddMenuElement(PartsMenu,label='Flip Horizontally',underline=5)
-        self.parent.FlipPartVerticallyDoer.AddMenuElement(PartsMenu,label='Flip Vertically',underline=5)
-
-        WireMenu=Menu(self)
-        self.add_cascade(label='Wires',menu=WireMenu,underline=0)
-        self.parent.AddWireDoer.AddMenuElement(WireMenu,label='Add Wire',underline=0)
-        WireMenu.add_separator()
-        self.parent.DeleteVertexDoer.AddMenuElement(WireMenu,label='Delete Vertex',underline=7)
-        self.parent.DuplicateVertexDoer.AddMenuElement(WireMenu,label='Duplicate Vertex',underline=1)
-        self.parent.DeleteWireDoer.AddMenuElement(WireMenu,label='Delete Wire',underline=0)
-
-        ViewMenu=Menu(self)
-        self.add_cascade(label='View',menu=ViewMenu,underline=0)
-        self.parent.ZoomInDoer.AddMenuElement(ViewMenu,label='Zoom In',underline=5)
-        self.parent.ZoomOutDoer.AddMenuElement(ViewMenu,label='Zoom Out',underline=5)
-        self.parent.PanDoer.AddMenuElement(ViewMenu,label='Pan',underline=0)
-
-        CalcMenu=Menu(self)
-        self.add_cascade(label='Calculate',menu=CalcMenu,underline=0)
-        self.parent.CalculationPropertiesDoer.AddMenuElement(CalcMenu,label='Calculation Properties',underline=12)
-        self.parent.SParameterViewerDoer.AddMenuElement(CalcMenu,label='S-parameter Viewer',underline=12)
-        CalcMenu.add_separator()
-        self.parent.CalculateSParametersDoer.AddMenuElement(CalcMenu,label='Calculate S-parameters',underline=0)
-        self.parent.SimulateDoer.AddMenuElement(CalcMenu,label='Simulate',underline=0)
-
-class ToolBar(Frame):
-    def __init__(self,parent):
-        self.parent=parent
-        Frame.__init__(self,self.parent)
-        self.pack(side=TOP,fill=X,expand=NO)
-        self.parent.ClearProjectDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/document-new-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.OpenProjectDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/document-open-2.gif',).Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.SaveProjectDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/document-save-2.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        separator=Frame(self,bd=2,relief=SUNKEN)
-        separator.pack(side=LEFT,fill=X,padx=5,pady=5)
-        self.parent.AddPartDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/edit-add-2.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.DeleteSelectedDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/edit-delete-6.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.AddWireDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/draw-line-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.DuplicateSelectedDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/edit-copy-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.RotatePartDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/object-rotate-left-4.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.FlipPartHorizontallyDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/object-flip-horizontal-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.FlipPartVerticallyDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/object-flip-vertical-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        separator=Frame(self,height=2,bd=2,relief=RAISED).pack(side=LEFT,fill=X,padx=5,pady=5)
-        self.parent.ZoomInDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/zoom-in-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.ZoomOutDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/zoom-out-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.PanDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/edit-move.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        separator=Frame(self,height=2,bd=2,relief=RAISED).pack(side=LEFT,fill=X,padx=5,pady=5)
-        self.parent.CalculationPropertiesDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/tooloptions.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-        self.parent.CalculateDoer.AddToolBarElement(self,iconfile='./icons/png/16x16/actions/system-run-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
-
-        undoFrame=Frame(self)
-        undoFrame.pack(side=RIGHT,fill=NONE,expand=NO,anchor=E)
-        self.parent.UndoDoer.AddToolBarElement(undoFrame,iconfile='./icons/png/16x16/actions/edit-undo-3.gif').Pack(side=LEFT,fill=NONE,expand=NO,anchor=E)
-        self.parent.RedoDoer.AddToolBarElement(undoFrame,iconfile='./icons/png/16x16/actions/edit-redo-3.gif').Pack(side=LEFT,fill=NONE,expand=NO,anchor=E)
-
-class StatusBar(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        self.label = Label(self, bd=1, relief=SUNKEN, anchor=W)
-        self.label.pack(fill=X)
-    def set(self, format, *args):
-        self.label.config(text=format % args)
-        self.label.update_idletasks()
-    def clear(self):
-        self.label.config(text="")
-        self.label.update_idletasks()
+from MenuSystemHelpers import *
 
 class TheApp(Frame):
     def __init__(self):
@@ -240,8 +52,10 @@ class TheApp(Frame):
                 else:
                     exit()
 
+        # status bar
         self.statusbar=StatusBar(self)
 
+        # the Doers - the holder of the commands, menu elements, toolbar elements, and key bindings
         self.UndoDoer = Doer(self.onUndo).AddKeyBindElement(self.root,'<Control-z>')
         self.RedoDoer = Doer(self.onRedo).AddKeyBindElement(self.root,'<Control-Z>')
         self.OpenProjectDoer = Doer(self.onReadProjectFromFile).AddKeyBindElement(self.root,'<Control-o>')
@@ -249,7 +63,7 @@ class TheApp(Frame):
         self.ClearProjectDoer = Doer(self.onClearSchematic)
         self.ExportNetListDoer = Doer(self.onExportNetlist)
         self.ExportTpXDoer = Doer(self.onExportTpX)
-
+        # ------
         self.AddPartDoer = Doer(self.onAddPart)
         self.AddPortDoer = Doer(self.onAddPort)
         self.DeletePartDoer = Doer(self.onDeletePart)
@@ -260,35 +74,116 @@ class TheApp(Frame):
         self.RotatePartDoer = Doer(self.onRotatePart)
         self.FlipPartHorizontallyDoer = Doer(self.onFlipPartHorizontally)
         self.FlipPartVerticallyDoer = Doer(self.onFlipPartVertically)
-
+        # ------
         self.AddWireDoer = Doer(self.onAddWire)
         self.DeleteVertexDoer = Doer(self.onDeleteSelectedVertex)
         self.DuplicateVertexDoer = Doer(self.onDuplicateSelectedVertex)
         self.DeleteWireDoer = Doer(self.onDeleteSelectedWire)
-
+        # ------
         self.ZoomInDoer = Doer(self.onZoomIn)
         self.ZoomOutDoer = Doer(self.onZoomOut)
         self.PanDoer = Doer(self.onPan)
-
+        # ------
         self.CalculationPropertiesDoer = Doer(self.onCalculationProperties)
         self.SParameterViewerDoer = Doer(self.onSParameterViewer)
         self.CalculateDoer = Doer(self.onCalculate)
         self.CalculateSParametersDoer = Doer(self.onCalculateSParameters)
         self.SimulateDoer = Doer(self.onSimulate)
 
-        self.menu=TheMenu(self)
-        self.toolbar=ToolBar(self)
+        # The menu system
+        TheMenu=Menu(self.root)
+        self.root.config(menu=TheMenu)
+        FileMenu=Menu(self)
+        TheMenu.add_cascade(label='File',menu=FileMenu,underline=0)
+        self.OpenProjectDoer.AddMenuElement(FileMenu,label="Open Project",accelerator='Ctrl+O',underline=0)
+        self.SaveProjectDoer.AddMenuElement(FileMenu,label="Save Project",accelerator='Ctrl+S',underline=0)
+        FileMenu.add_separator()
+        self.ClearProjectDoer.AddMenuElement(FileMenu,label="Clear Schematic",underline=0)
+        FileMenu.add_separator()
+        self.ExportNetListDoer.AddMenuElement(FileMenu,label="Export NetList",underline=7)
+        self.ExportTpXDoer.AddMenuElement(FileMenu,label="Export LaTeX (TikZ)",underline=7)
+        # ------
+        EditMenu=Menu(self)
+        TheMenu.add_cascade(label='Edit',menu=EditMenu,underline=0)
+        self.UndoDoer.AddMenuElement(EditMenu,label="Undo",accelerator='Ctrl+Z', underline=0)
+        self.RedoDoer.AddMenuElement(EditMenu,label="Redo",accelerator='Ctrl+Shift+Z',underline=0)
+        EditMenu.add_separator()
+        # ------
+        PartsMenu=Menu(self)
+        TheMenu.add_cascade(label='Parts',menu=PartsMenu,underline=0)
+        self.AddPartDoer.AddMenuElement(PartsMenu,label='Add Part',underline=0)
+        self.AddPortDoer.AddMenuElement(PartsMenu,label='Add Port',underline=5)
+        PartsMenu.add_separator()
+        self.DeletePartDoer.AddMenuElement(PartsMenu,label='Delete Part',underline=0)
+        PartsMenu.add_separator()
+        self.EditPropertiesDoer.AddMenuElement(PartsMenu,label='Edit Properties',underline=0)
+        self.DuplicatePartDoer.AddMenuElement(PartsMenu,label='Duplicate Part',accelerator='Ctrl+C',underline=0)
+        self.RotatePartDoer.AddMenuElement(PartsMenu,label='Rotate Part',underline=0)
+        self.FlipPartHorizontallyDoer.AddMenuElement(PartsMenu,label='Flip Horizontally',underline=5)
+        self.FlipPartVerticallyDoer.AddMenuElement(PartsMenu,label='Flip Vertically',underline=5)
+        # ------
+        WireMenu=Menu(self)
+        TheMenu.add_cascade(label='Wires',menu=WireMenu,underline=0)
+        self.AddWireDoer.AddMenuElement(WireMenu,label='Add Wire',underline=0)
+        WireMenu.add_separator()
+        self.DeleteVertexDoer.AddMenuElement(WireMenu,label='Delete Vertex',underline=7)
+        self.DuplicateVertexDoer.AddMenuElement(WireMenu,label='Duplicate Vertex',underline=1)
+        self.DeleteWireDoer.AddMenuElement(WireMenu,label='Delete Wire',underline=0)
+        # ------
+        ViewMenu=Menu(self)
+        TheMenu.add_cascade(label='View',menu=ViewMenu,underline=0)
+        self.ZoomInDoer.AddMenuElement(ViewMenu,label='Zoom In',underline=5)
+        self.ZoomOutDoer.AddMenuElement(ViewMenu,label='Zoom Out',underline=5)
+        self.PanDoer.AddMenuElement(ViewMenu,label='Pan',underline=0)
+        # ------
+        CalcMenu=Menu(self)
+        TheMenu.add_cascade(label='Calculate',menu=CalcMenu,underline=0)
+        self.CalculationPropertiesDoer.AddMenuElement(CalcMenu,label='Calculation Properties',underline=12)
+        self.SParameterViewerDoer.AddMenuElement(CalcMenu,label='S-parameter Viewer',underline=12)
+        CalcMenu.add_separator()
+        self.CalculateSParametersDoer.AddMenuElement(CalcMenu,label='Calculate S-parameters',underline=0)
+        self.SimulateDoer.AddMenuElement(CalcMenu,label='Simulate',underline=0)
 
+        # The Toolbar
+        ToolBarFrame = Frame(self)
+        ToolBarFrame.pack(side=TOP,fill=X,expand=NO)
+        self.ClearProjectDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/document-new-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.OpenProjectDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/document-open-2.gif',).Pack(side=LEFT,fill=NONE,expand=NO)
+        self.SaveProjectDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/document-save-2.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        Frame(ToolBarFrame,bd=2,relief=SUNKEN).pack(side=LEFT,fill=X,padx=5,pady=5)
+        self.AddPartDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/edit-add-2.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.DeleteSelectedDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/edit-delete-6.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.AddWireDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/draw-line-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.DuplicateSelectedDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/edit-copy-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.RotatePartDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/object-rotate-left-4.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.FlipPartHorizontallyDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/object-flip-horizontal-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.FlipPartVerticallyDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/object-flip-vertical-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        Frame(ToolBarFrame,height=2,bd=2,relief=RAISED).pack(side=LEFT,fill=X,padx=5,pady=5)
+        self.ZoomInDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/zoom-in-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.ZoomOutDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/zoom-out-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.PanDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/edit-move.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        Frame(ToolBarFrame,height=2,bd=2,relief=RAISED).pack(side=LEFT,fill=X,padx=5,pady=5)
+        self.CalculationPropertiesDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/tooloptions.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        self.CalculateDoer.AddToolBarElement(ToolBarFrame,iconfile='./icons/png/16x16/actions/system-run-3.gif').Pack(side=LEFT,fill=NONE,expand=NO)
+        # ------
+        UndoFrame=Frame(ToolBarFrame)
+        UndoFrame.pack(side=RIGHT,fill=NONE,expand=NO,anchor=E)
+        self.UndoDoer.AddToolBarElement(UndoFrame,iconfile='./icons/png/16x16/actions/edit-undo-3.gif').Pack(side=LEFT,fill=NONE,expand=NO,anchor=E)
+        self.RedoDoer.AddToolBarElement(UndoFrame,iconfile='./icons/png/16x16/actions/edit-redo-3.gif').Pack(side=LEFT,fill=NONE,expand=NO,anchor=E)
+
+        # The Drawing (which contains the schecmatic)
         self.Drawing=Drawing(self)
         self.Drawing.pack(side=TOP,fill=BOTH,expand=YES)
 
         self.statusbar.pack(side=BOTTOM,fill=X,expand=NO)
         self.root.bind('<Key>',self.onKey)
 
+        # The Simulator Dialog
         self.simulator = Simulator(self)
         self.calculationProperties=CalculationProperties(self)
         self.filename=None
 
+        # The edit history (for undo)
         self.history=History(self)
 
         self.root.mainloop()
