94a489b6f9e6139f6233581788abd2e8f2fb465b
PetePupalaikis <peterp@lecroy.com>
2016-02-07 12:34:45 -0500

    updated pi mixed-mode termination

--- TestSignalIntegrity/TestMixedModeTermination.py
+++ TestSignalIntegrity/TestMixedModeTermination.py
@@ -67,17 +67,17 @@ class TestMixedModeTermination(unittest.TestCase,RoutineWriterTesterHelper,Respo
         # pragma: include
         sdp=si.p.SystemDescriptionParser().AddLines([
             'device MM 4 mixedmode',
-            'device R1 2',
-            'device R3 1',
-            'device R2 1',
-            'connect MM 1 R1 2 R2 1',
-            'connect MM 2 R1 1 R3 1',
+            'device Z3 2',
+            'device Z1 1',
+            'device Z2 1',
+            'connect MM 1 Z3 2 Z1 1',
+            'connect MM 2 Z3 1 Z2 1',
             'port 1 MM 3',
             'port 2 MM 4'])
         ssps=si.sd.SystemSParametersSymbolic(sdp.SystemDescription(),size='small')
-        ssps.AssignSParameters('R1',si.sy.SeriesZ('Z_1'))
-        ssps.AssignSParameters('R2',si.sy.ShuntZ(1,'Z_2'))
-        ssps.AssignSParameters('R3',si.sy.ShuntZ(1,'Z_3'))
+        ssps.AssignSParameters('Z3',si.sy.SeriesZ('Z_3'))
+        ssps.AssignSParameters('Z1',si.sy.ShuntZ(1,'Z_1'))
+        ssps.AssignSParameters('Z2',si.sy.ShuntZ(1,'Z_2'))
         ssps.LaTeXSolution(size='big')
         ssps.Emit()
         # pragma: exclude
