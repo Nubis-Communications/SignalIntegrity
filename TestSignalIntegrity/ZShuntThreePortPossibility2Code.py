96304f1a8351f20e05cf7255f536f1d9763a6ca1
PetePupalaikis <peterp@lecroy.com>
2016-04-08 17:14:49 -0400

    updated code examples for shuntz changes in last commit

--- TestSignalIntegrity/ZShuntThreePortPossibility2Code.py
+++ TestSignalIntegrity/ZShuntThreePortPossibility2Code.py
@@ -4,5 +4,5 @@ sdp=si.p.SystemDescriptionParser()
 sdp.AddLines(['device D 4','device O 1 open','port 1 D 1 2 D 3 3 D 2',
     'connect D 4 O 1'])
 ssps=si.sd.SystemSParametersSymbolic(sdp.SystemDescription(),size='small')
-ssps.AssignSParameters('D',si.sy.ShuntZFourPort('Z'))
+ssps.AssignSParameters('D',si.sy.ShuntZ(4,'Z'))
 ssps.LaTeXSolution(size='big').Emit()
--- TestSignalIntegrity/ZShuntTwoPortPossibility2Code.py
+++ TestSignalIntegrity/ZShuntTwoPortPossibility2Code.py
@@ -4,5 +4,5 @@ sdp=si.p.SystemDescriptionParser()
 sdp.AddLines(['device D 3','device G 1 ground',
     'port 1 D 1 2 D 2','connect D 3 G 1'])
 ssps=si.sd.SystemSParametersSymbolic(sdp.SystemDescription())
-ssps.AssignSParameters('D',si.sy.ShuntZThreePort('Z'))
+ssps.AssignSParameters('D',si.sy.ShuntZ(3,'Z'))
 ssps.LaTeXSolution(size='big').Emit()
