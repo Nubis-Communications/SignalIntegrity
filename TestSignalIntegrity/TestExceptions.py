dc47e807888cc5d761a8d5fdfe3d0d8b3f38479c
PetePupalaikis <peterp@lecroy.com>
2018-02-04 12:56:16 -0500

    check connections for empty netlist
    and test

--- TestSignalIntegrity/TestExceptions.py
+++ TestSignalIntegrity/TestExceptions.py
@@ -20,6 +20,14 @@ class TestExceptions(unittest.TestCase,SParameterCompareHelper):
                 pass
     def testSystemDescriptionCheckConnections2(self):
         sdp=si.p.SystemDescriptionParser()
+        ssps=si.sd.SystemSParametersSymbolic(sdp.SystemDescription())
+        try:
+            ssps.LaTeXSolution().Emit()
+        except si.PySIException as e:
+            if e == si.PySIExceptionSystemDescription:
+                pass
+    def testSystemDescriptionCheckConnections3(self):
+        sdp=si.p.SystemDescriptionParser()
         sdp.AddLines(['device DV 4','device ZI 2','device ZO 2',
             'port 1 ZI 1 2 ZI 2 3 ZO 2 4 DV 3',
             'connect ZI 1 DV 2','connect ZI 2 DV 1'])
