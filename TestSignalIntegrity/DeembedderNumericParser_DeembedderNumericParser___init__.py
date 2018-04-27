29b07a250e48fbdfb2b39aa5b5513aaa05ece9d7
PeterP <pete_pope@hotmail.com>
2018-04-27 17:38:33 -0400

    smaller code

--- TestSignalIntegrity/DeembedderNumericParser_DeembedderNumericParser___init__.py
+++ TestSignalIntegrity/DeembedderNumericParser_DeembedderNumericParser___init__.py
@@ -9,18 +9,12 @@ class DeembedderNumericParser(DeembedderParser,CallBacker):
         for n in range(len(self.m_f)):
             system=None
             for d in range(len(self.m_spc)):
-                if self.m_spc[d][0] == 'system':
-                    system=self.m_spc[d][1][n]
-                else:
-                    self.m_sd.AssignSParameters(self.m_spc[d][0],
-                        self.m_spc[d][1][n])
-            if not systemSParameters is None:
-                system=systemSParameters[n]
+                if self.m_spc[d][0] == 'system': system=self.m_spc[d][1][n]
+                else: self.m_sd.AssignSParameters(self.m_spc[d][0],self.m_spc[d][1][n])
+            if not systemSParameters is None: system=systemSParameters[n]
             unl=DeembedderNumeric(self.m_sd).CalculateUnknown(system)
             if NumUnknowns == 1: unl=[unl]
-            for u in range(NumUnknowns):
-                result[u].append(unl[u])
+            for u in range(NumUnknowns): result[u].append(unl[u])
         sf=[SParameters(self.m_f,r) for r in result]
-        if len(sf)==1:
-            return sf[0]
+        if len(sf)==1: return sf[0]
         return sf
