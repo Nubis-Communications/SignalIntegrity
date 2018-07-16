6cc55321284d21bfa6fea2419a86c730189bfa5f
PetePupalaikis <peterp@lecroy.com>
2018-07-16 16:25:32 -0400

    fix help by embedding css

--- PySIApp/Help/Convert.sh
+++ PySIApp/Help/Convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python /usr/bin/elyxer --title "PySI Help" --splitpart 3  --nofooter --toc --notoclabels PySIHelp.lyx PySIHelp.html.LyXconv/PySIHelp.html
+python /usr/bin/elyxer --title "PySI Help" --embedcss lyx.css --splitpart 3  --nofooter --toc --notoclabels PySIHelp.lyx PySIHelp.html.LyXconv/PySIHelp.html
 
