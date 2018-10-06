11d8b134119f463bbda3cf6edb0757432e1a025d
PeterP <pete_pope@hotmail.com>
2018-10-06 12:51:46 -0400

    Help System Changes
    moved the entire help system to pages

---
+++ PySIApp/Help/Convert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python ~/Work/elyxer-1.2.5/elyxer.py --title "PySI Help" --embedcss lyx.css --splitpart 3  --nofooter --toc --notoclabels PySIHelp.lyx PySIHelp.html.LyXconv/PySIHelp.html
+
