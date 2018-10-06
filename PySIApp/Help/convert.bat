8c8b0ec8f474ed520fba872127e71e87d315a43d
PetePupalaikis <peterp@lecroy.com>
2018-10-06 12:53:05 -0400

    Help System Moved

--- PySIApp/Help/convert.bat
+++
@@ -1,2 +0,0 @@
-python c:\work\elyxer-1.2.5\elyxer.py --title "PySI Help" --splitpart 3  --nofooter --toc --notoclabels PySIHelp.lyx PySIHelp.html
-START /WAIT explorer PySIHelp.html
--- PySIApp/Help/convert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python /usr/bin/elyxer --title "PySI Help" --embedcss lyx.css --splitpart 3  --nofooter --toc --notoclabels PySIHelp.lyx PySIHelp.html.LyXconv/PySIHelp.html
-
