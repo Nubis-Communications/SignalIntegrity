e679f137b7d7f400d608d79edde7dafc50e7a5a8
PeterP <pete_pope@hotmail.com>
2018-11-12 20:38:15 -0500

    workin on installer
    very frustrating but working now
    needs more testing.
    resolves #23.

--- SignalIntegrity/App/About.py
+++ SignalIntegrity/App/About.py
@@ -61,19 +61,22 @@ class CreditsDialog(Toplevel):
 class LicenseDialog(Toplevel):
     def __init__(self,parent):
         self.parent=parent
-        textToShow = []
-        try:
-            licenseFile=open(parent.parent.installdir+'/../../LICENSE.txt','rU' if sys.version_info.major < 3 else 'r')
-            for line in licenseFile:
-                if line.strip()=='':
-                    textToShow.append('\n')
-                else:
-                    textToShow=textToShow+textwrap.wrap(line,80)
-            licenseFile.close()
-
-        except IOError:
-            textToShow = ['LICENSE FILE NOT FOUND']
-
+        textToShow=[
+            'Copyright (c) 2018 Teledyne LeCroy, Inc.',
+            'All rights reserved worldwide.',
+            '',
+            'This file is part of SignalIntegrity.',
+            '',
+            'SignalIntegrity is free software: You can redistribute it and/or modify it under the terms',
+            'of the GNU General Public License as published by the Free Software Foundation, either',
+            'version 3 of the License, or any later version.',
+            '',
+            'This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;',
+            'without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.',
+            'See the GNU General Public License for more details.',
+            '',
+            'You should have received a copy of the GNU General Public License along with this program.',
+            'If not, see <https://www.gnu.org/licenses/>']
         Toplevel.__init__(self, parent)
         self.img = PhotoImage(file=parent.parent.installdir+'/icons/png/AppIcon2.gif')
         self.gnuimg=PhotoImage(file=parent.parent.installdir+'/icons/png/gpl.gif')
