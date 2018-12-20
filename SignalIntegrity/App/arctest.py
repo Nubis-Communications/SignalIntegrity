d09233dc2ccfde68393c4700aa5b9923c26f99e4
PeterP <pete_pope@hotmail.com>
2018-12-20 11:42:23 -0500

    fixed python3 bug in arctest

--- SignalIntegrity/App/arctest.py
+++ SignalIntegrity/App/arctest.py
@@ -17,7 +17,11 @@ arctest.py
 #
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see <https://www.gnu.org/licenses/>
+import sys
+if sys.version_info.major < 3:
     import Tkinter as tk
+else:
+    import tkinter as tk
 
 def onCalculate():
     canvas.delete(tk.ALL)
