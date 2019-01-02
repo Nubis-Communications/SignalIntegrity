c57923dc745c2fb6414a7361af2ded652adbc5f5
PetePupalaikis <pete_pope@hotmail.com>
2019-01-02 06:29:50 -0500

    version 1.1.1

--- Doc/xhtml/search/all_8.js
+++ Doc/xhtml/search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['h',['h',['../classSignalIntegrity_1_1Lib_1_1Wavelets_1_1Wavelets_1_1Wavelet.xhtml#a5e36941b3d856737e81516acd45edc50',1,'SignalIntegrity::Lib::Wavelets::Wavelets::Wavelet']]],
-  ['hashvalue',['HashValue',['../classSignalIntegrity_1_1Lib_1_1ResultsCache_1_1LinesCache.xhtml#afdb5d6ee41e3d7027d9890bb207daecd',1,'SignalIntegrity::Lib::ResultsCache::LinesCache']]]
+  ['hashvalue',['HashValue',['../classSignalIntegrity_1_1Lib_1_1ResultsCache_1_1LinesCache.xhtml#afdb5d6ee41e3d7027d9890bb207daecd',1,'SignalIntegrity::Lib::ResultsCache::LinesCache']]],
+  ['header',['header',['../classSignalIntegrity_1_1Lib_1_1SParameters_1_1SParameters_1_1SParameters.xhtml#ae8f626807b2dddc1872b3005556939cd',1,'SignalIntegrity::Lib::SParameters::SParameters::SParameters']]]
 ];
