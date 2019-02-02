6fbf2080c3140fd254917e0af8eda8d6a194ae53
PetePupalaikis <pete_pope@hotmail.com>
2019-02-02 15:49:15 -0500

    updated setup information
    for release

--- SignalIntegrity/__about__.py
+++ SignalIntegrity/__about__.py
@@ -29,4 +29,4 @@ __url__ = "https://github.com/TeledyneLeCroy/SignalIntegrity"
 __copyright__ = u"Copyright {} 2018, {}".format(u"\u00A9",__organization__)
 __license__ = "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
 __version__ = "1.1.2"
-__status__ = "Development Status :: 4 - Beta"
+__status__ = "Development Status :: 5 - Production/Stable"
