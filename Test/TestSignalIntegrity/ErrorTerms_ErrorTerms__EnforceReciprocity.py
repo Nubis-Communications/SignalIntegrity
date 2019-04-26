7c6d8cf3f680bfcc77e8fdfc94111eb7876d2304
PetePupalaikis <pete_pope@hotmail.com>
2019-04-25 22:32:17 -0400

    reciprocity enforcement

--- Test/TestSignalIntegrity/ErrorTerms_ErrorTerms__EnforceReciprocity.py
+++ Test/TestSignalIntegrity/ErrorTerms_ErrorTerms__EnforceReciprocity.py
@@ -6,7 +6,7 @@ class ErrorTerms(object):
         for c in Pr:
             for r in Pr:
                 M[r][c]=M[c][r] if r < c else r-c+(0 if c==0 else M[P-1][c-1]+1)
-        L=[[0. for c in range(M[P-1][P-1]+1)] for r in range(P*P)]
+        L=[[0. for c in range(P*(P+1)//2)] for r in range(P*P)]
         b=[None for r in range(P*P)]
         for r in Pr:
             for c in Pr:
