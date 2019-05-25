27b960e48800585bb661e97bb5ca406c213d71cb
PetePupalaikis <pete_pope@hotmail.com>
2019-05-25 19:44:09 -0400

    Zero forcing code

--- Test/TestSignalIntegrity/ZeroForcingEqualizerCode.py
+++ Test/TestSignalIntegrity/ZeroForcingEqualizerCode.py
@@ -1,6 +1,5 @@
 def ZeroForcingEqualizer(project,waveform,bitrate,value,pre,taps):
-    import SignalIntegrity.App as siapp
-    from numpy import matrix
+    import SignalIntegrity.App as siapp; from numpy import matrix
     app=siapp.SignalIntegrityAppHeadless()
     app.OpenProjectFile(project)
     (_,outputWaveformLabels,_,outputWaveformList)=app.Simulate()
@@ -10,8 +9,7 @@ def ZeroForcingEqualizer(project,waveform,bitrate,value,pre,taps):
     H=pulsewf.td.H; ui=1./bitrate
     startTime=delay-(int((delay-H)/ui)-1)*ui
     endTime=delay+(int((pulsewf.Times()[-1]-delay)/ui-1)*ui)
-    M=int((endTime-startTime)/ui+0.5)
-    d=int((delay-startTime)/ui+0.5)
+    M=int((endTime-startTime)/ui+0.5); d=int((delay-startTime)/ui+0.5)
     x=[pulsewf.Measure(startTime+m*ui) for m in range(M)]
     X=[[0 if r-c < 0 else x[r-c] for c in range(taps)] for r in range(M)]
     r=[[value] if r == d+pre else [0] for r in range(len(X))]
