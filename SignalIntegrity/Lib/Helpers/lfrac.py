a72661ded0088355581c53772777bfc1e193588a
PetePupalaikis <pete_pope@hotmail.com>
2019-09-20 19:53:36 -0400

    forgot frac routine itself

---
+++ SignalIntegrity/Lib/Helpers/lfrac.py
@@ -0,0 +1,25 @@
+"""
+lfrac.py
+"""
+
+# Copyright (c) 2018 Teledyne LeCroy, Inc.
+# All rights reserved worldwide.
+#
+# This file is part of SignalIntegrity.
+#
+# SignalIntegrity is free software: You can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>
+np='\\vphantom{\\frac{a}{b}}'
+np=''
+dp='\\vphantom{X^{2}}'
+dp=''
+def lfrac(a,b):
+    return '\\frac{'+np+a+'}{'+dp+b+'}'
