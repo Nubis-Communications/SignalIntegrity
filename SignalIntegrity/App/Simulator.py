b186e85c99d7fb3ee1440e268918f1a73129ceb2
PetePupalaikis <pete_pope@hotmail.com>
2020-02-06 10:36:01 -0500

    Plot sizes for simulator dialog as well

--- SignalIntegrity/App/Simulator.py
+++ SignalIntegrity/App/Simulator.py
@@ -124,11 +124,16 @@ class SimulatorDialog(tk.Toplevel):
         self.ControlHelpDoer.AddToolBarElement(ToolBarFrame,iconfile=iconsdir+'help-3.gif').Pack(side=tk.LEFT,fill=tk.NONE,expand=tk.NO)
 
         labelFrame = tk.Frame(self)
-        labelFrame.pack(side=tk.TOP,fill=tk.X,expand=tk.YES)
+        labelFrame.pack(side=tk.TOP,fill=tk.X,expand=tk.NO)
         self.plotLabel = tk.Label(labelFrame,fg='black')
         self.plotLabel.pack(fill=tk.X)
 
-        self.f = Figure(figsize=(6,4), dpi=100)
+        plotWidth=SignalIntegrity.App.Preferences['Appearance.PlotWidth']
+        plotHeight=SignalIntegrity.App.Preferences['Appearance.PlotHeight']
+        plotDPI=SignalIntegrity.App.Preferences['Appearance.PlotDPI']
+
+        self.f = Figure(figsize=(plotWidth,plotHeight), dpi=plotDPI)
+
         self.plt = self.f.add_subplot(111)
         self.plt.set_xlabel('time (ns)')
         self.plt.set_ylabel('amplitude')
@@ -137,7 +142,7 @@ class SimulatorDialog(tk.Toplevel):
         self.waveformNamesList=None
         self.canvas = FigureCanvasTkAgg(self.f, master=self)
         #canvas.show()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.X, expand=1)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES)
 
         toolbar = NavigationToolbar2Tk( self.canvas, self )
         toolbar.update()
