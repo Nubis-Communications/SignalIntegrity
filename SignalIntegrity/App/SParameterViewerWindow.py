eeff8ca088b55da53746b5ed0ba520bde5618ee0
PetePupalaikis <pete_pope@hotmail.com>
2020-04-17 18:47:41 -0400

    improvements to multiple s-parameter display

--- SignalIntegrity/App/SParameterViewerWindow.py
+++ SignalIntegrity/App/SParameterViewerWindow.py
@@ -389,7 +389,7 @@ class SParametersDialog(tk.Toplevel):
         # ------
         self.SelectionMenu.delete(0, tk.END)
         for s in range(len(self.spList)):
-            self.SelectionDoerList[s].AddMenuElement(self.SelectionMenu,label=self.spList[s][2])
+            self.SelectionDoerList[s].AddMenuElement(self.SelectionMenu,label=FileParts(self.spList[s][1]).FileNameTitle())
         self.TheMenu.entryconfigure('Selection', state= tk.DISABLED if len(self.spList) <= 1 else tk.ACTIVE)
         # ------
 
@@ -399,7 +399,7 @@ class SParametersDialog(tk.Toplevel):
         self.UpdatePropertiesFromSParameters(new=True)
 
         # button labels are a proxy for transfer parameters (until I do something better)
-        areSParameters=(buttonLabels == None)
+        areSParameters=(buttonLabels == [['s'+str(toP+1)+str(fromP+1) for fromP in range(sp.m_P)] for toP in range(sp.m_P)])
         isCalibration=(self.calibration != None)
         self.ShowPassivityViolationsDoer.Activate(areSParameters)
         self.ShowCausalityViolationsDoer.Activate(areSParameters)
@@ -969,6 +969,7 @@ class SParametersDialog(tk.Toplevel):
 
         if spChanged:
             self.UpdatePropertiesFromSParameters()
+            self.spList[self.currentlySelected][0]=self.sp
         if not msg is None:
             msg.destroy()
         if spChanged:
@@ -1444,7 +1445,7 @@ class SParametersDialog(tk.Toplevel):
             sp=si.sp.SParameterFile(filename)
 
         buttonLabels=[['s'+str(toP+1)+str(fromP+1) for fromP in range(sp.m_P)] for toP in range(sp.m_P)]
-        spList=self.spList+[[sp,filename,'S-Parameters',buttonLabels]]
+        spList=self.spList+[[sp,self.fileparts.FileNameTitle(),'S-Parameters',buttonLabels]]
         self.NewSParameters(spList)
 
     def onWriteSParametersToFile(self):
@@ -1654,6 +1655,7 @@ class SParametersDialog(tk.Toplevel):
 
     def onSelection(self,x):
         self.sp=self.spList[x][0]
+        self.currentlySelected=x
         self.properties=SParameterProperties()
         self.UpdatePropertiesFromSParameters(new=True)
         filename=self.spList[x][1]
