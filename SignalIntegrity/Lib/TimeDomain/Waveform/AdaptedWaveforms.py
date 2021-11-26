7f1d46e6b33b81fcda3e663efdc58f154de50ad3
PetePupalaikis <pete_pope@hotmail.com>
2021-11-26 12:17:52 -0500

    Adapted Waveforms Fix
    See comments in changed file

--- SignalIntegrity/Lib/TimeDomain/Waveform/AdaptedWaveforms.py
+++ SignalIntegrity/Lib/TimeDomain/Waveform/AdaptedWaveforms.py
@@ -31,12 +31,20 @@ class AdaptedWaveforms(list):
         from SignalIntegrity.Lib.TimeDomain.Filters.InterpolatorSinX import FractionalDelayFilterSinX
         from SignalIntegrity.Lib.TimeDomain.Filters.InterpolatorLinear import InterpolatorLinear
         from SignalIntegrity.Lib.TimeDomain.Filters.InterpolatorLinear import FractionalDelayFilterLinear
+        from SignalIntegrity.Lib.Rat import Rat
         strategy=wfl[0].adaptionStrategy
+
         #upsample all of the waveforms first
-        ufl=[int(round(wfl[0].td.Fs/wf.td.Fs)) for wf in wfl]
-        wfl=[wf if uf == 1 else wf*
-            (InterpolatorSinX(uf) if strategy=='SinX' else InterpolatorLinear(uf))
-            for (uf,wf) in zip(ufl,wfl)]
+        # udfl is a list of (U,D) tuples containing upsample and decimation factor
+        # todo: this change was made to prevent a failure, presumably when decimation was needed (and would
+        # create an integer upsample factor of 0).  Now, for example, it might have an upsample factor of 1 and
+        # a downsample factor of 2, instead of 0 for the integer upsample factor, which caused failure.  But, it's
+        # unclear how the downsample factor is handled downstream of this and this needs to be investigated.
+        # in any case, all the unit tests pass with this change.
+        udfl=[Rat(wfl[0].td.Fs/wf.td.Fs) for wf in wfl]
+        wfl=[wf if uf[0] == 1 else wf*
+            (InterpolatorSinX(uf[0]) if strategy=='SinX' else InterpolatorLinear(uf[0]))
+            for (uf,wf) in zip(udfl,wfl)]
         adl=[wfl[0].td/wf.td for wf in wfl]
         fdl=[ad.D-int(ad.D) for ad in adl]
         wfl=[wf if fd == 0.0 else wf*
