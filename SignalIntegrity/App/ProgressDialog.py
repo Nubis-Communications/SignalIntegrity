f4a87a33b9edd5e0684b3ef7c8f7ce28a6f3b2ef
ashokbruno <ashokbruno@gmail.com>
2022-10-12 19:15:58 -0400

    fix tkinter issues

--- SignalIntegrity/App/ProgressDialog.py
+++ SignalIntegrity/App/ProgressDialog.py
@@ -35,7 +35,7 @@ class ProgressDialog(tk.Toplevel):
         self.transient(parent)
         self.attributes('-topmost',True)
         self.title(title)
-        self.img = tk.PhotoImage(file=SignalIntegrity.App.IconsBaseDir+'AppIcon2.gif')
+        self.img = tk.PhotoImage(file=SignalIntegrity.App.IconsBaseDir+'AppIcon2.gif', master=self)
         self.tk.call('wm', 'iconphoto', self._w, self.img)
         self.protocol("WM_DELETE_WINDOW", self.destroy)
         self.barFrame=tk.Frame(self)
