58368d94acebe7e60c1e41f51e35240d2868d5ba
PetePupalaikis <pete_pope@hotmail.com>
2023-03-20 07:27:07 -0400

    v1.3.2

--- Doc/xhtml/search/functions_b.js
+++ Doc/xhtml/search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['makedevice_1154',['MakeDevice',['../classSignalIntegrity_1_1Lib_1_1Parsers_1_1Devices_1_1DeviceParser_1_1DeviceFactory.xhtml#a84bd6828768b3a1677c71d39f3cb91dd',1,'SignalIntegrity::Lib::Parsers::Devices::DeviceParser::DeviceFactory']]],
+  ['makedevice_1154',['MakeDevice',['../classSignalIntegrity_1_1Lib_1_1Parsers_1_1Devices_1_1DeviceParser_1_1DeviceFactory.xhtml#ac166a957897ea098010f081c4e58d09f',1,'SignalIntegrity::Lib::Parsers::Devices::DeviceParser::DeviceFactory']]],
   ['maxwellmatrix_1155',['MaxwellMatrix',['../classSignalIntegrity_1_1Lib_1_1SParameters_1_1Devices_1_1WElement_1_1MutualMatrix.xhtml#aa5907d0fcdbef820ea286264a120a409',1,'SignalIntegrity::Lib::SParameters::Devices::WElement::MutualMatrix']]],
   ['measure_1156',['Measure',['../classSignalIntegrity_1_1Lib_1_1Eye_1_1EyeDiagramBitmap_1_1EyeDiagramBitmap.xhtml#aca020d6bf17e9ab0b799b5543c2cb201',1,'SignalIntegrity.Lib.Eye.EyeDiagramBitmap.EyeDiagramBitmap.Measure()'],['../classSignalIntegrity_1_1Lib_1_1TimeDomain_1_1Waveform_1_1Waveform_1_1Waveform.xhtml#a3ebba9b24670eb206a63950249f9b964',1,'SignalIntegrity.Lib.TimeDomain.Waveform.Waveform.Waveform.Measure()']]],
   ['mixedmodeconverter_1157',['MixedModeConverter',['../namespaceSignalIntegrity_1_1Lib_1_1Devices_1_1MixedModeConverter.xhtml#a08d8fcc7bca48ded65f2e917f803d4cc',1,'SignalIntegrity::Lib::Devices::MixedModeConverter']]],
