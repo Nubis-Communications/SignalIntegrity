412da29b95d6184c23acd14a815fe96f17d7a338
PetePupalaikis <pete_pope@hotmail.com>
2024-04-11 12:57:07 -0400

    Added DC phase fix to list of enforcement possibilities

--- SignalIntegrity/App/SParameterPropertiesDialog.py
+++ SignalIntegrity/App/SParameterPropertiesDialog.py
@@ -107,9 +107,10 @@ class SParameterPropertiesDialog(PropertiesDialog):
         pass
 
     def onPositiveTimeLimitEntered(self,event):
-        if self.project['TimeLimitPositive']<0.:
-            self.project['TimeLimitPositive']=0.
-            self.positiveTime.UpdateStrings()
+#         if self.project['TimeLimitPositive']<0.:
+#             self.project['TimeLimitPositive']=0.
+#             self.positiveTime.UpdateStrings()
+        pass
 
     def onReferenceImpedanceEntered(self,event):
         self.UpdateStrings()
